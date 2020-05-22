fecfe930c3ec1852f84e1145f90641b523b51065
toriSSi <jooh21020@naver.com>
2020-05-22 22:33:34 +0900

    10강 repsitory 구현하여 중복 코드 제거

--- restaurant-book/restaurant-api/src/main/java/com/example/restaurant/interfaces/RestaurantController.java
+++ restaurant-book/restaurant-api/src/main/java/com/example/restaurant/interfaces/RestaurantController.java
@@ -1,38 +1,33 @@
 package com.example.restaurant.interfaces;
 
 import com.example.restaurant.domain.Restaurant;
+import com.example.restaurant.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository reopsitory = new RestaurantRepository();
+
     @GetMapping("/restaurant")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        Restaurant restaurant = new Restaurant(1004L, "Bab zip", "Seoul");
-
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = reopsitory.findAll();
 
         return restaurants;
     }
 
     @GetMapping("/restaurant/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L, "Bab zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber food", "Seoul"));
+        Restaurant restaurant = reopsitory.findById(id);
 
-        Restaurant restaurant = restaurants.stream() //리스트 중에서
-                .filter(r -> r.getId().equals(id)) //r의 아이디가 패스변수의 id와 같은 것을 찾고
-                .findFirst() //그 중 첫 번째의
-                .get(); //값을 가져와 or .orElse(null); 못찾을 경우 null을 가져와. => 못찾으면 원래는 예외가 발생함
- 
         return restaurant;
     }
 }
+
+// 레파지토리를 이용하여 컨트롤러가 간단해짐
+// ui 레이어는 사용자와 내부에 있는 비즈니스로직(도메인로직)이 서로 상관 없고 징검다리 역할만하고 
+// 실제 비즈니스 로직은 도메인 레이어에서 함
