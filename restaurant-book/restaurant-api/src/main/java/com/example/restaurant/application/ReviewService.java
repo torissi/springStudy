feab45909c7a5b98c7253f798b66e88b4cf80685
toriSSi <jooh21020@gmail.com>
2020-06-08 21:53:17 +0900

    26강 리뷰 작성

---
+++ restaurant-book/restaurant-api/src/main/java/com/example/restaurant/application/ReviewService.java
@@ -0,0 +1,10 @@
+package com.example.restaurant.application;
+
+import com.example.restaurant.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public void addReview(Review review) {
+    }
+}
